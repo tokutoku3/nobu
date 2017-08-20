6d73a73af2026db9d9933e4832e15f9de68cb69a
Tokuyama Takuya <tokuyama@camobile.com>
2017-08-21 00:22:40 +0900

    package.jsonの更新

--- scripts/trello.js
+++ scripts/trello.js
@@ -1,5 +1,28 @@
+// @link https://developers.trello.com/v1.0/reference
 var Trello = require('node-trello');
 var moment = require('moment');
+
+/**
+ * 確認用
+ * listのidを一覧で表示する
+ */
+controller.hears(['trello_list_id'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    var trello = new Trello(process.env.TRELLO_APPLICATION_KEY, process.env.TRELLO_USER_TOKEN);
+
+    trello.get('1/boards/9iLCpyg3/lists', function(err, data) {
+        if (err) {
+            throw err;
+        }
+
+        var list = '```\n';
+        data.forEach(function(d) {
+            list += d.name + ': ' + d.id + '\n';
+        });
+        list += '```\n';
+
+        bot.reply(message, list);
+    });
+});
 
 /**
  * 冷蔵庫内の在庫確認
@@ -8,6 +31,7 @@ controller.hears(['冷蔵庫*'], ['direct_message', 'direct_mention', 'mention']
     var trello = new Trello(process.env.TRELLO_APPLICATION_KEY, process.env.TRELLO_USER_TOKEN);
     var foodsListId = process.env.TRELLO_FOODS_LIST_ID;
     var seasoningListId = process.env.TRELLO_SEASONING_LIST_ID;
+    var instantListId = process.env.TRELLO_INSTANT_LIST_ID;
 
     bot.reply(message, '食料の備蓄はこんなかんじじゃぞ！');
 
@@ -25,6 +49,14 @@ controller.hears(['冷蔵庫*'], ['direct_message', 'direct_mention', 'mention']
             throw err;
         }
         bot.reply(message, createStockList('*■調味料*', data));
+    });
+
+    // インスタント一覧
+    trello.get('1/lists/' + instantListId + '/cards', function(err, data) {
+        if (err) {
+            throw err;
+        }
+        bot.reply(message, createStockList('*■インスタント*', data));
     });
 });
 
