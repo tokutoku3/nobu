f1ca12bf1120c9c59f3127349ca03813e3373a80
tokutoku3 <toku3.w@gmail.com>
2017-08-30 23:10:44 +0900

    Added: pingサンプル追加

--- scripts/ping.js
+++ scripts/ping.js
@@ -4,3 +4,12 @@
 controller.hears(['ping'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
     bot.reply(message, 'PONG');
 });
+
+controller.hears(['はろー'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    bot.replyWithTyping(message, 'うむ！');
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'thumbsup'
+    });
+});
